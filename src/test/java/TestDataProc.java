f1263620745cd8ca156e42d93b661feb7d381eb8
Arno Hollosi <ahollosi@xmp.net>
2022-05-01 11:27:00 +0200

    #2 add 3 test cases for sumDuration

---
+++ src/test/java/TestDataProc.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
+
+public class TestDataProc {
+    @Test
+    public void testSum1() {
+        DataProcessor dp = new DataProcessor();
+        Date prev = new Date(122, Calendar.MARCH, 1, 12, 55);
+        Date cur = new Date(122, Calendar.MARCH, 1, 13, 15);
+        dp.sumDuration("abc", prev, cur);
+        Assert.assertEquals(Duration.ofMinutes(20), dp.getResult().get("abc"));
+    }
+
+    @Test
+    public void testSum2() {
+        DataProcessor dp = new DataProcessor();
+        Date prev = new Date(122, Calendar.MARCH, 1, 12, 55);
+        Date cur = new Date(122, Calendar.MARCH, 1, 13, 15);
+        Date next = new Date(122, Calendar.MARCH, 1, 13, 40);
+        dp.sumDuration("abc", prev, cur);
+        dp.sumDuration("abc", cur, next);
+        Assert.assertEquals(Duration.ofMinutes(20+25), dp.getResult().get("abc"));
+    }
+
+    @Test
+    public void testSum3() {
+        DataProcessor dp = new DataProcessor();
+        Date cur = new Date(122, Calendar.MARCH, 1, 12, 55);
+        dp.sumDuration("abc", null, cur);
+        Assert.assertFalse(dp.getResult().containsKey("abc"));
+    }
+}
